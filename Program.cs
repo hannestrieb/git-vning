5b4c7b3a3780eefd55b1ba176273fa4f49541ea9
Philip Johansson <philip@onefamily.se>
2026-01-07 14:48:22 +0100

    philips ändringar

--- Program.cs
+++ Program.cs
@@ -6,9 +6,15 @@ class Program
     {
         int result = Plus(2, 8);
         Console.WriteLine($"The result is: {result}");
+        int result2 = Minus(10, 4);
+        Console.WriteLine($"The result is: {result2}");
     }
     static int Plus(int a, int b)
     {
         return a + b;
+    }
+    static int Minus(int a, int b)
+    {
+        return a - b;
     }
 }
