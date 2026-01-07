0d7d17f60a6538d64734199ff42e7dbfbf31e7e3
hannestrieb <triebhannes@gmail.com>
2026-01-07 14:38:54 +0100

    la till mer filer

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿namespace gitövning;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int result = Plus(2, 8);
+        Console.WriteLine($"The result is: {result}");
+    }
+    static int Plus(int a, int b)
+    {
+        return a + b;
+    }
+}
